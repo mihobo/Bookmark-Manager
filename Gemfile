07599453eb3b0989ae62dcca890fb607e1a9bb2b
Miho Tse <mihotse@gmail.com>
2017-06-27 12:09:42 +0100

    Explain CRUD, SQL & Create a link model

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.4.0'
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'dm-migrations'
+gem 'rspec'
+gem 'capybara'
+gem 'sinatra'
+gem 'thin'
---
+++ app.rb
@@ -0,0 +1,9 @@
+# require 'sinatra/base'
+# require './app/models/link.rb'
+#
+# class BookmarkManager < Sinatra::Base
+#
+#   # get '/links' do
+#   #   'www.google.com'
+#   # end
+# end
