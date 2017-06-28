8360657c6221affa44c7978bf4a64a05aedc42bb
Miho Tse <mihotse@gmail.com>
2017-06-28 22:34:26 +0100

    Merge duplicated view link spec files

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,5 +1,14 @@
 # spec/features/viewing_links_spec.rb
 feature 'Viewing links' do
+  scenario 'see a list of exisitng links on the homepage' do
+    Link.create(url: 'http://www.google.com', title: 'Google')
+    visit('/links')
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
+  end
+  
   before(:each) do
     Link.create(url: 'http://www.bbc.co.uk', title: 'BBC', tags: [Tag.first_or_create(name: 'News')])
     Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'Browser')])
